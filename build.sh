a3a6e160381f460a3c5a1da54716bb6fb5d5b9f0
vinicostaalves95-ops <vinicostaalves95@gmail.com>
2026-05-19 16:45:24 -0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 set -e
 pip install -r requirements.txt
-python3 -m modal deploy modal_app.py --token-id $MODAL_TOKEN_ID --token-secret $MODAL_TOKEN_SECRET
+MODAL_TOKEN_ID=$MODAL_TOKEN_ID MODAL_TOKEN_SECRET=$MODAL_TOKEN_SECRET python3 -m modal deploy modal_app.py
