afe43892b99ce565a30ae801a83ee639139b4081
vinicostaalves95-ops <vinicostaalves95@gmail.com>
2026-05-19 16:48:25 -0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,13 @@
 #!/usr/bin/env bash
 set -e
 pip install -r requirements.txt
+
+# Cria config do Modal com o token
+mkdir -p ~/.modal
+cat > ~/.modal/credentials.toml << EOF
+[default]
+token_id = "${MODAL_TOKEN_ID}"
+token_secret = "${MODAL_TOKEN_SECRET}"
+EOF
+
+python3 -m modal deploy modal_app.py
